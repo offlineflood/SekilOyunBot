03cef6d9d1ec836386f90ef4b55487d79f3e21b4
offlineflood <108521240+offlineflood@users.noreply.github.com>
2022-07-26 19:42:20 +0400

    Update botTesla.js

--- botTesla.js
+++ botTesla.js
@@ -167,7 +167,7 @@ const RaundMesajHusnuEhedov = (chatId, round, time) => {
 	})
 	answers = answers.sort((a, b) => oyunDurumuHusnuEhedov[chatId].answersOrder.indexOf(a.memberId) - oyunDurumuHusnuEhedov[chatId].answersOrder.indexOf(b.memberId))
 
-	return Degisken(` * 🆚➪ Raund ${round + 1}/${process.env.RAUND_SAYI} *	❔ Sizcə bu adam neçə yaşındadır.${answers.length > 0 ? ` \n${answers.map((member, index) => `${index + 1}. *${member.firstName}*: ${member.answer}` ).join("\n")}\n`	:	""	}${"⚡️".repeat(time)}${"✨".repeat(config.emojiSaniye - time)}`)}
+	return Degisken(`  🆚➪ Raund ${round + 1}/${process.env.RAUND_SAYI} 	\n\n❔ Sizcə bu adam neçə yaşındadır.${answers.length > 0 ? ` \n${answers.map((member, index) => `${index + 1}. *${member.firstName}*: ${member.answer}` ).join("\n")}\n`	:	""	}${"⚡️".repeat(time)}${"✨".repeat(config.emojiSaniye - time)}`)}
 
 const OyunHusnuEhedov = (ctx, chatId) => {
 	let gameState = OyunYaratHusnuEhedov(chatId)
